24e6ea20da3f29f28ec89c26ce6075ec14925273
Panferov.creator <panferov.creator@gmail.com>
2023-06-22 13:22:34 +0500

    Initial Commit

---
+++ Airport/AirportApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirportApp.swift
+//  Airport
+//
+//  Created by Dmitriy Panferov on 22/06/23.
+//
+
+import SwiftUI
+
+@main
+struct AirportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
